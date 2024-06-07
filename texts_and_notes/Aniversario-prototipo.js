db7bf855485bc4dd91a8a9bcd59bbfc3cdeffaff
Traian <traian@tutanota.com>
2024-06-06 23:02:42 -0300

    flasdrive copies

---
+++ texts_and_notes/Aniversario-prototipo.js
@@ -0,0 +1,95 @@
+var parabenizar = function (nome, sobrenome, sobresobrenome, idade, Classe, type, threatLevel, alignment)
+{
+   
+var nome = "ian"
+var sobrenome = "o brabo das interwebs"
+var sobresobrenome = "hackerman, o invadido, o desprovido de privacidade"
+var idade = "infinito, eterno (35 para leigos e desinformados)"
+var Classe = "Humano, semi-cracudo"
+var type = "Inteligente, compassivo, instrutor natural, barba foda"
+var threatLevel ="Over 9000"
+var alignment  = "Caotico Bom"
+
+console.log("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦")
+
+console.log("Operador:" +' '+nome +' '+sobrenome+' ');
+
+console.log("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦")
+
+console.log("tambem conhecido como:" +' '+sobresobrenome+' ');
+
+console.log("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦")
+
+console.log("﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼")
+
+console.log("Classe:" + ' ' + Classe)
+
+console.log("﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼﷼")
+
+console.log("♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾")
+
+console.log("idade:" + ' ' + idade)
+
+console.log("♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾-♾")
+
+console.log("☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬")
+
+console.log("Traços principais:" + ' ' + type)
+
+console.log("☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬☬")
+
+console.log("☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯")
+
+console.log("Alinhamento" +' ' + alignment)
+
+console.log("☯☯☯☯☯☯☯☯☯☯☯☯☯☯☯")
+
+console.log("☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢")
+
+console.log("nivel de ameaça global:" + ' ' + threatLevel)
+
+console.log("☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢☢")
+
+
+/* mais uma vez desculpa o espaçamento espero que rode legal aí
+feliz aniversario */
+
+
+/* as coisas ficaram com espaçamento zuado pq foi feito durante o trabalho e com pressa
+nao queria que passasse muito tempo de eu começar e de enviar pra voce */
+/* gosto muito do comando console.log como dá pra ver */
+/* eventualmente eu vou aprender arrays então ta tudo certo */
+
+/*anotações ou coisa do tipo dicas tudo bem vindo mas lembra eu to começando agora e isso não é lá 
+tão familiar pra mim  */
+}
+
+parabenizar()
+/*Uma nota é que sim fiz da maneira menos eficiente pq eu quero praticar bastante e fixar na cabeça pra ir 
+para as "contrações" que simplificam sabendo ja de raiz oq eu tô encurtando 
+obviamente algo bem bobinho que eu quis fazer */
+
+/* caso esteja vendo a parte de dentro
+feliz aniversario eu te amo muito obrigado por tudo
+fica bem e se respeita */
+
+/* Pra mim você é imortal pois carrego você comigo enquanto eu respirar esse ar filho da puta que a gente tem */
+/* Te amo cara */
+console.log("∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴")
+console.log("tem um texto dentro do arquivo tbm caso queira ver")
+/* haha vou fazer a maquina falar pau no seu cu */
+/* MAQUINA EU TE ORDENO, MANDA ELE TOMA NO CU */
+console.log("Vai toma no seu cu")
+/* kkkkkk mó onda, se cuida te amo */
+console.log("esse vai toma no cu tem contexto") 
+console.log("∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴∴")
+
+/* espero que goste e tenha chance de ver isso, desculpa não ter feito nada mais legal
+to meio curto em recursos kkkkk */
+
+/* so quis assinar msm liga não */
+console.log("☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭")
+
+console.log("☭[Isso foi feito por alguem... -T ]☭☭☭☭☭")
+
+console.log("☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭")
